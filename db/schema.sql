b76e88731bc76325f5b70641466f94b763cd6584
Mark Bernstein <ProgrammingByMark@gmail.com>
2019-12-30 16:57:13 -0800

    starting point for project, all files creates

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers
+(
+id int NOT NULL AUTO_INCREMENT,
+
+burger_name VARCHAR(50) NOT NULL,
+
+devoured BOOLEAN DEFAULT false,        
+
+PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', false);
