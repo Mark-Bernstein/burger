f7d9da05490ffeea10b3efb0e782a84f9a85165b
Mark Bernstein <ProgrammingByMark@gmail.com>
2019-12-31 15:44:36 -0800

    added img successfully

--- config/orm.js
+++ config/orm.js
@@ -60,7 +60,7 @@ var orm = {
     queryString += printQuestionMarks(vals.length);
     queryString += ") ";
 
-    console.log(queryString);
+    console.log("creating burger (queryString)", queryString);
 
     connection.query(queryString, vals, function(err, result) {
       if (err) {
@@ -79,7 +79,7 @@ var orm = {
     queryString += " WHERE ";
     queryString += condition;
 
-    console.log(queryString);
+    console.log("updating burger (queryString)", queryString);
     connection.query(queryString, function(err, result) {
       if (err) {
         console.log(err);
--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -23,61 +23,57 @@ router.get("/burgers", function(req, res) {
 });
 
 
-
-router.post("/burgers/create", function(req, res) {
-  burger.create(["burger_name", "devoured"], 
-  [
-    req.body.burger_name, req.body.devoured
-  ], function(result) {
-    // wrapper for orm.js that using MySQL insert callback will return a log to console,
-    // render back to index with handle
-    console.log("post console log here!!!", result);
-    res.redirect("/");
-  });
-});
-
-// // post route -> back to index
 // router.post("/burgers/create", function(req, res) {
-//   // takes the request object using it as input for burger.addBurger
-//   burger.create(req.body.burger_name, function(result) {
+//   burger.create(["burger_name", "devoured"], 
+//   [
+//     req.body.burger_name, req.body.devoured
+//   ], function(result) {
 //     // wrapper for orm.js that using MySQL insert callback will return a log to console,
 //     // render back to index with handle
-//     console.log(result);
+//     console.log("post console log here!!!", result);
 //     res.redirect("/");
 //   });
 // });
 
-
-
-
-router.put("burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.update({
-    devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+// post route -> back to index
+router.post("/burgers/create", function(req, res) {
+  // takes the request object using it as input for burger.addBurger
+  burger.create(req.body.burger_name, function(result) {
+    // wrapper for orm.js that using MySQL insert callback will return a log to console,
+    // render back to index with handle
+    console.log(result);
+    res.redirect("/");
   });
 });
 
 
-// // put route -> back to index
-// router.put("/burgers/:id", function(req, res) {
-//   burger.update(req.params.id, function(result) {
-//     // wrapper for orm.js that using MySQL update callback will return a log to console,
-//     // render back to index with handle
-//     console.log(result);
-//     // Send back response and let page reload from .then in Ajax
-//     res.sendStatus(200);
+// router.put("burgers/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
+
+//   console.log("condition", condition);
+
+//   burger.update({ devoured: req.body.devoured }, condition, function(result) {
+//     if (result.changedRows == 0) {
+//       // If no rows were changed, then the ID must not exist, so 404
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
 //   });
 // });
+
+
+// put route -> back to index
+router.put("/burgers/:id", function(req, res) {
+  burger.update(req.params.id, function(result) {
+    // wrapper for orm.js that using MySQL update callback will return a log to console,
+    // render back to index with handle
+    console.log(result);
+    // Send back response and let page reload from .then in Ajax
+    res.sendStatus(200);
+    //res.redirect("/");
+  });
+});
 
 
 // Export routes for server.js to use.
